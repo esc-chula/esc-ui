5b5f33ead837d77523a981529bba4a86a328c405
ChanatpakornD <csirintronsopon8@gmail.com>
2024-08-21 18:41:57 +0700

    chore: radix-checkbox added

---
+++ lib/components/Checkbox/index.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Checkbox } from '.'
+
+const meta: Meta<typeof Checkbox> = {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
