9f4b7ccd6db6283412ab79ba01cb0bbe1d83b134
ChanatpakornD <csirintronsopon8@gmail.com>
2024-08-28 00:50:44 +0700

    refactor: avatar struct

--- lib/components/Avatar/index.stories.tsx
+++ lib/components/Avatar/index.stories.tsx
@@ -1,17 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { Avatar, AvatarFallback, AvatarImage } from '.'
+import { Avatar } from '.'
 
 const meta: Meta<typeof Avatar> = {
   title: 'Components/Avatar',
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  render: (args) => (
-    <Avatar>
-      <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
-      <AvatarFallback>Shadcn</AvatarFallback>
-    </Avatar>
-  ),
-
+  component: Avatar,
   parameters: {
     layout: 'centered',
   },
@@ -23,22 +16,14 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Image: Story = {
-  args: {},
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  render: (args) => (
-    <Avatar>
-      <AvatarImage src='https://github.com/shadcn.png' alt='@shadcn' />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
-  ),
+  args: {
+    src: 'https://www.boomchanotai.com/boom.png',
+    alt: 'Boom Chanotai',
+    fallback: 'BC',
+  },
 }
 export const Fallback: Story = {
-  args: {},
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  render: (args) => (
-    <Avatar>
-      <AvatarImage src='' alt='@shadcn' />
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
-  ),
+  args: {
+    fallback: 'BC',
+  },
 }
