b04667ccb7065ccea92e8f0836a29587cf858ec7
ChanatpakornD <csirintronsopon8@gmail.com>
2024-08-28 13:50:18 +0700

    feat: add more props

--- lib/components/Textarea/index.tsx
+++ lib/components/Textarea/index.tsx
@@ -4,24 +4,43 @@ import { cn } from '@/utils'
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
-  title: string
+  label?: string
+  htmlFor?: string
+  placeholder?: string
   remarkText?: string
 }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, title, remarkText, ...props }, ref) => {
+  (
+    {
+      className,
+      htmlFor,
+      label,
+      placeholder = 'Insert your text here',
+      remarkText,
+      ...props
+    },
+    ref
+  ) => {
     return (
       <div className='grid w-full gap-1.5'>
-        <label htmlFor={title}>{title}</label>
+        {label && (
+          <label htmlFor={htmlFor} className={cn('text-sm')}>
+            {label}
+          </label>
+        )}
         <textarea
           className={cn(
             'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
             className
           )}
           ref={ref}
+          placeholder={placeholder}
           {...props}
         />
-        <p className='text-sm text-muted-foreground'>{remarkText}</p>
+        {remarkText && (
+          <p className='text-sm text-muted-foreground'>{remarkText}</p>
+        )}
       </div>
     )
   }
