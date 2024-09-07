cbbe985744e75f94579d17cf1ed98a7395eb3281
PhorDotC <por2547.c@gmail.com>
2024-09-07 20:23:03 +0700

    chore: change value  control

--- lib/components/Progress/index.stories.tsx
+++ lib/components/Progress/index.stories.tsx
@@ -6,6 +6,11 @@ const meta: Meta<typeof Progress> = {
   title: 'Components/Progress',
   component: Progress,
   tags: ['autodocs'],
+  argTypes: {
+    value: {
+      control: { type: 'range', min: 0, max: 100, step: 1 },
+    },
+  },
 }
 
 export default meta
