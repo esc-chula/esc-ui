37c81c22e915a014f360e4f557428eebea452eeb
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-09-09 22:27:38 +0700

    style: update color in radio components

--- lib/components/Radio/index.tsx
+++ lib/components/Radio/index.tsx
@@ -26,13 +26,13 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        'aspect-square h-4 w-4 rounded-full border border-[#E5E5E5] ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        'aspect-square h-4 w-4 rounded-full border border-neutral-200 ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
         className
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className='flex items-center justify-center'>
-        <Circle className='size-2.5 fill-[#262626] text-current' />
+        <Circle className='size-2.5 fill-neutral-800 text-current' />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
