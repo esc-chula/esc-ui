d314a1269de9db6f8e4b9d2995353f3d18285c33
CSAP <113345557+ChanatpakornS@users.noreply.github.com>
2024-10-02 16:42:49 +0700

    Feature/check box (#4)
    * chore: radix-checkbox added
    
    * feat: added label
    
    * Feature/switch (#3)
    
    * feat: switch component
    
    * feat: switch component
    
    * fix: pnpm lock
    
    ---------
    
    Co-authored-by: boomchanotai <boom9387@hotmail.com>
    
    ---------
    
    Co-authored-by: boomchanotai <boom9387@hotmail.com>
    Co-authored-by: Chatdanai Porncharoensub <116288526+PhorDotC@users.noreply.github.com>

---
+++ lib/components/Checkbox/index.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Checkbox } from '.'
+
+const meta: Meta<typeof Checkbox> = {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    label: 'Accept terms and conditions',
+  },
+}
+
+export const Checked: Story = {
+  args: {},
+}
