3bb22832cbfc6c84d597797b8a21f4531a60a9c7
Chatdanai Porncharoensub <116288526+PhorDotC@users.noreply.github.com>
2024-10-02 16:39:57 +0700

    Feature/switch (#3)
    * feat: switch component
    
    * feat: switch component
    
    * fix: pnpm lock
    
    ---------
    
    Co-authored-by: boomchanotai <boom9387@hotmail.com>

---
+++ lib/components/Switch/index.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Switch } from '.'
+
+const meta: Meta<typeof Switch> = {
+  title: 'Components/Switch',
+  component: Switch,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+}
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
