1874de8659e1ae83b119c20b7cc07e5402693fdb
Phongit Khanthawisood (Tonnam) <phongit.ton@gmail.com>
2024-10-14 21:51:53 +0700

    feat: add args in slider storybook

--- lib/components/Slider/index.stories.tsx
+++ lib/components/Slider/index.stories.tsx
@@ -2,8 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Slider } from '@/components/Slider'
 
-import { cn } from '@/utils'
-
 type SliderProps = React.ComponentProps<typeof Slider>
 
 const meta: Meta<typeof Slider> = {
@@ -13,23 +11,74 @@ const meta: Meta<typeof Slider> = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    defaultValue: {
+      control: { array: { type: 'number' } },
+      description: 'The initial value(s) of the slider',
+      defaultValue: [50], // Default to 50 for the preview
+    },
+    max: {
+      control: 'number',
+      description: 'The maximum value for the slider',
+      defaultValue: 100, // Default maximum value
+    },
+    step: {
+      control: 'number',
+      description: 'The step size for the slider',
+      defaultValue: 1, // Default step size
+    },
+    disabled: {
+      control: 'boolean',
+      description: 'Disable the slider',
+      defaultValue: false, // Slider enabled by default
+    },
+  },
 }
 
 export default meta
 
 type Story = StoryObj<typeof meta>
-
+// Default Slider Story with args controls
 export const Default: Story = {
-  args: {},
-  render: ({ className, ...props }: SliderProps) => (
+  args: {
+    defaultValue: [50], // Default starting value
+    max: 100, // Maximum value of the slider
+    step: 1, // Step size
+    disabled: false, // Slider is enabled by default
+  },
+  render: (args: SliderProps) => (
     <div className='flex w-[625px] items-center justify-center p-10'>
-      <Slider
-        defaultValue={[50]}
-        max={100}
-        step={1}
-        className={cn('w-[60%]', className)}
-        {...props}
-      />
+      <Slider {...args} className='w-3/5' />
+    </div>
+  ),
+}
+
+// Custom Range Slider Story
+export const CustomRange: Story = {
+  args: {
+    defaultValue: [30],
+    max: 200,
+    step: 10,
+    disabled: false,
+  },
+  render: (args: SliderProps) => (
+    <div className='flex w-[625px] items-center justify-center p-10'>
+      <Slider {...args} className='w-3/5' />
+    </div>
+  ),
+}
+
+// Disabled Slider Story
+export const Disabled: Story = {
+  args: {
+    defaultValue: [70],
+    max: 100,
+    step: 1,
+    disabled: true,
+  },
+  render: (args: SliderProps) => (
+    <div className='flex w-[625px] items-center justify-center p-10'>
+      <Slider {...args} className='w-3/5' />
     </div>
   ),
 }
