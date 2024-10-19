469c8b2269b7700921d261e732524f23d0df6387
ChanatpakornD <csirintronsopon8@gmail.com>
2024-10-19 12:51:40 +0700

    refactor: delete X comment from components

--- lib/components/Dialog/index.tsx
+++ lib/components/Dialog/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import * as DialogPrimitive from '@radix-ui/react-dialog'
-// import { X } from 'lucide-react'
 import * as React from 'react'
 
 import { cn } from '@/utils'
@@ -44,10 +43,6 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      {/* <DialogPrimitive.Close className='data-[state=open]:bg-white data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 ring-offset-neutral-300 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-carmine-500 focus:ring-offset-2 disabled:pointer-events-none'>
-        <X className='h-4 w-4' />
-        <span className='sr-only'>Close</span>
-      </DialogPrimitive.Close> */}
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
