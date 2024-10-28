0b7d96186ab0fb9270568a42d25813c59882115c
boomchanotai <boom9387@hotmail.com>
2024-10-28 11:11:54 +0700

    feat: upgrade storybook

--- lib/components/AlertDialog/index.tsx
+++ lib/components/AlertDialog/index.tsx
@@ -91,7 +91,7 @@ const AlertDialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AlertDialogPrimitive.Description
     ref={ref}
-    className={cn('text-muted-foreground text-sm', className)}
+    className={cn('text-sm text-neutral-500', className)}
     {...props}
   />
 ))
